cc5dbf5e78d5133acc30dd712301ac1738f324c0
ACE-RAZA <ACE-RAZA@ACE-RAZA-HP>
2014-02-16 20:27:20 +0500

    16th Feb!

--- codbr/omod/src/main/java/org/openmrs/module/codbr/fragment/controller/content/EnterHtmlFormFragmentController.java
+++ codbr/omod/src/main/java/org/openmrs/module/codbr/fragment/controller/content/EnterHtmlFormFragmentController.java
@@ -1,101 +1,183 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
 package org.openmrs.module.codbr.fragment.controller.content;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import org.openmrs.Encounter;
-import org.openmrs.Form;
-import org.openmrs.Patient;
-import org.openmrs.api.FormService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.*;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.htmlformentry.FormEntryContext;
 import org.openmrs.module.htmlformentry.FormEntryContext.Mode;
 import org.openmrs.module.htmlformentry.FormEntrySession;
+import org.openmrs.module.htmlformentry.FormSubmissionError;
 import org.openmrs.module.htmlformentry.HtmlForm;
-import org.openmrs.module.htmlformentry.HtmlFormEntryService;
 import org.openmrs.module.htmlformentry.HtmlFormEntryUtil;
-import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.fragment.FragmentActionRequest;
 import org.openmrs.ui.framework.fragment.FragmentConfiguration;
 import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.openmrs.ui.framework.page.PageRequest;
 import org.openmrs.ui.framework.resource.ResourceFactory;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ *  Controller for HTML Form Entry form submissions
+ */
 public class EnterHtmlFormFragmentController {
-	
-    public void controller(FragmentConfiguration config,
-           /* UiSessionContext sessionContext,*/
-            UiUtils ui,
-            @SpringBean("htmlFormEntryService") HtmlFormEntryService htmlFormEntryService,
-            @SpringBean("formService") FormService formService,
-           /* @SpringBean("coreResourceFactory") ResourceFactory resourceFactory,
-            @SpringBean("featureToggles") FeatureToggleProperties featureToggles,*/
-            @FragmentParam("patient") Patient patient,
-            @FragmentParam(value = "htmlForm", required = false) HtmlForm hf,
-            @FragmentParam(value = "htmlFormId", required = false) Integer htmlFormId,
-            @FragmentParam(value = "formId", required = false) Form form,
-            @FragmentParam(value = "formUuid", required = false) String formUuid,
-            @FragmentParam(value = "definitionUiResource", required = false) String definitionUiResource,
-            @FragmentParam(value = "encounter", required = false) Encounter encounter,
-            /*@FragmentParam(value = "visit", required = false) VisitDomainWrapper visit,*/
-            @FragmentParam(value = "createVisit", required = false) Boolean createVisit,
-            @FragmentParam(value = "returnUrl", required = false) String returnUrl,
-            @FragmentParam(value = "automaticValidation", defaultValue = "true") boolean automaticValidation,
-            FragmentModel model,
-            HttpSession httpSession) throws Exception {
 
-		config.require("patient",
-				"htmlForm | htmlFormId | formId | formUuid | definitionResource | encounter");
-		htmlFormId = 1;
-		if (hf == null) {
-			if (htmlFormId != null) {
-				hf = htmlFormEntryService.getHtmlForm(htmlFormId);
-			} else if (form != null) {
-				hf = htmlFormEntryService.getHtmlFormByForm(form);
-			} else if (formUuid != null) {
-				form = formService.getFormByUuid(formUuid);
-				hf = htmlFormEntryService.getHtmlFormByForm(form);
-			}
-		}
-		if (hf == null && encounter != null) {
-			form = encounter.getForm();
-			if (form == null) {
-				throw new IllegalArgumentException(
-						"Cannot view a form-less encounter unless you specify which form to use");
-			}
-			hf = HtmlFormEntryUtil.getService().getHtmlFormByForm(
-					encounter.getForm());
-			if (hf == null)
-				throw new IllegalArgumentException(
-						"The form for the specified encounter ("
-								+ encounter.getForm()
-								+ ") does not have an HtmlForm associated with it");
-		}
-		if (hf == null)
-			throw new RuntimeException("Could not find HTML Form");
+	protected final Log log = LogFactory.getLog(EnterHtmlFormFragmentController.class);
 
-		// the code below doesn't handle the HFFS case where you might want to
-		// _add_ data to an existing encounter
+	public void controller(/*@FragmentParam("patient") Patient patient,*/
+						   @FragmentParam(value = "formUuid", required = false) String formUuid,
+						   @FragmentParam(value = "encounter", required = false) Encounter encounter,
+						   @FragmentParam(value = "visit", required = false) Visit visit,
+						   @FragmentParam(value = "returnUrl", required = false) String returnUrl,
+						   FragmentConfiguration config,
+						   FragmentModel model,
+						   HttpSession httpSession,
+						   PageRequest pageRequest,
+						   @SpringBean ResourceFactory resourceFactory) throws Exception {
+
+
+		
+		Patient patient = Context.getPatientService().getPatient(2);
+		// Get html form from database or UI resource
+		HtmlForm hf = HtmlFormEntryUtil.getService().getHtmlForm(1);
+		
+
+
+		// The code below doesn't handle the HFFS case where you might want to _add_ data to an existing encounter
 		FormEntrySession fes;
 		if (encounter != null) {
-			fes = new FormEntrySession(patient, encounter, Mode.EDIT, hf,
-					httpSession);
-		} else {
+			fes = new FormEntrySession(patient, encounter, Mode.EDIT, hf, httpSession);
+		}
+		else {
 			fes = new FormEntrySession(patient, hf, httpSession);
 		}
+
 		if (returnUrl != null) {
 			fes.setReturnUrl(returnUrl);
 		}
 
-		// Ensure we've generated the form's HTML (and thus set up the
-		// submission actions, etc) before we do anything
+		// Ensure we've generated the form's HTML (and thus set up the submission actions, etc) before we do anything
 		fes.getHtmlToDisplay();
 
-		// Context.setVolatileUserData(FORM_IN_PROGRESS_KEY, session);
+		//Context.setVolatileUserData(FORM_IN_PROGRESS_KEY, session);
 
 		model.addAttribute("command", fes);
+		model.addAttribute("visit", visit);
+	}
+
+	/**
+	 * Handles a form submission
+	 * @return form errors in a simple object
+	 * @throws Exception
+	 */
+	
+	public Object submit(@RequestParam("personId") Patient patient,
+						 @RequestParam("formId") Form form,
+						 @RequestParam(value = "encounterId", required = false) Encounter encounter,
+						 @RequestParam(value = "visitId", required = false) Visit visit,
+						 @RequestParam(value = "returnUrl", required = false) String returnUrl,
+						 @SpringBean ResourceFactory resourceFactory,
+												 FragmentActionRequest actionRequest) throws Exception {
+
+		// TODO formModifiedTimestamp and encounterModifiedTimestamp
+
+		
+
+		// Get html form from database or UI resource
+		HtmlForm hf = HtmlFormEntryUtil.getService().getHtmlForm(1);
+
+		FormEntrySession fes;
+		if (encounter != null) {
+			fes = new FormEntrySession(patient, encounter, Mode.EDIT, hf, actionRequest.getHttpRequest().getSession());
+		} else {
+			fes = new FormEntrySession(patient, hf, Mode.ENTER, actionRequest.getHttpRequest().getSession());
+		}
+
+		if (returnUrl != null) {
+			fes.setReturnUrl(returnUrl);
+		}
+
+		// Ensure we've generated the form's HTML (and thus set up the submission actions, etc) before we do anything
+		fes.getHtmlToDisplay();
+
+		// Validate submission
+		List<FormSubmissionError> validationErrors = fes.getSubmissionController().validateSubmission(fes.getContext(), actionRequest.getHttpRequest());
+
+		// If there are validation errors, abort submit and display them
+		if (validationErrors.size() > 0) {
+			return returnHelper(validationErrors, fes.getContext());
+		}
+
+		// No validation errors found so continue process of form submission
+		fes.prepareForSubmit();
+		fes.getSubmissionController().handleFormSubmission(fes, actionRequest.getHttpRequest());
+
+		// Check this form will actually create an encounter if its supposed to
+		if (fes.getContext().getMode() == Mode.ENTER && fes.hasEncouterTag() && (fes.getSubmissionActions().getEncountersToCreate() == null || fes.getSubmissionActions().getEncountersToCreate().size() == 0)) {
+			throw new IllegalArgumentException("This form is not going to create an encounter");
+		}
+
+		// Get the encounter that will be saved
+		Encounter formEncounter = fes.getContext().getMode() == Mode.ENTER ? fes.getSubmissionActions().getEncountersToCreate().get(0) : encounter;
 
 
-}
 
+		// Once again, if there are validation errors, abort submit and display them
+		if (validationErrors.size() > 0) {
+			return returnHelper(validationErrors, fes.getContext());
+		}
 
+		// Do actual encounter creation/updating
+		fes.applyActions();
+
+		return returnHelper(null, null);
+	}
+
+	/**
+	 *
+	 * @param validationErrors
+	 * @param context
+	 * @return
+	 */
+	private SimpleObject returnHelper(List<FormSubmissionError> validationErrors, FormEntryContext context) {
+		if (validationErrors == null || validationErrors.size() == 0) {
+			return SimpleObject.create("success", true);
+		} else {
+			Map<String, String> errors = new HashMap<String, String>();
+			for (FormSubmissionError err : validationErrors) {
+				if (err.getSourceWidget() != null)
+					errors.put(context.getErrorFieldId(err.getSourceWidget()), err.getError());
+				else
+					errors.put(err.getId(), err.getError());
+			}
+			return SimpleObject.create("success", false, "errors", errors);
+		}
+	}
 }
